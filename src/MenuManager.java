a1ae66f4b6eb22872af2a58085dacfdc239631ec
Robbie Tonos <Retonos@go.olemiss.edu>
2023-02-02 00:16:32 -0600

    Create MenuManager class to take away Menu Responsibility from Cafe

---
+++ src/MenuManager.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class MenuManager {
+
+    ArrayList<MenuItem> menuItems;
+
+    public void addItemToMenu(MenuItem menuItem){
+        menuItems.add(menuItem);
+    }
+
+}
