3a9047370ef520b37230023e9cb531d38704736a
Robbie Tonos <Retonos@go.olemiss.edu>
2023-02-01 23:33:23 -0600

    Add test for addToCustomerFavorites method

--- src/CafeTest.java
+++ src/CafeTest.java
@@ -85,7 +85,10 @@ class CafeTest {
     @Test
     void addtoCustomerFavorites(){
         Cafe cafe7 = new Cafe();
-
+        cafe7.addCustomer("Johnny", "Knoxville", "6629930293");
+        cafe7.addItemToMenu(new MenuItem("NewBrew", 4.50, 35, 145, "Large"));
+        cafe7.cafeCustomers.get(0).addToFavorites(cafe7.menuItems.get(0));
+        assertEquals(cafe7.getCustomer(0).customerFavorites.get(0).getChoiceName(), "NewBrew");
     }
 
 
