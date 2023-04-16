3dd8b577f8d34c6c601115f618ecc84672bb29ec
robbietonos37 <109938015+robbietonos37@users.noreply.github.com>
2023-04-16 02:56:50 -0500

    Add connectInfo for MenuManager constructor

--- src/MenuManager.java
+++ src/MenuManager.java
@@ -1,10 +1,32 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Properties;
 
 public class MenuManager {
 
     ArrayList<MenuItem> menuItems = new ArrayList<>();
 
     private int numberOfMenuItems = 0;
+
+    private Connection connection;
+    private String dbUrl;
+    private String dbUsername;
+    private String dbPassword;
+
+    public MenuManager() throws IOException, SQLException {
+        Properties props = new Properties();
+        props.load(new FileInputStream("config.properties"));
+        dbUrl = props.getProperty("db.url");
+        dbUsername = props.getProperty("db.username");
+        dbPassword = props.getProperty("db.password");
+
+        connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+    }
 
     public void addItemToMenu(MenuItem menuItem){
         menuItems.add(menuItem);
